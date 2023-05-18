13092dff7b5fd47c499f00ff6f61acecdb79c914
enricocapuano <80041693+enricocapuano@users.noreply.github.com>
2023-05-18 15:19:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class LightningModel(pl.LightningModule):
         return descriptors
 
     def configure_optimizers(self):
-         if self.opt_param == "sgd":
+        if self.opt_param == "sgd":
             optimizers = torch.optim.SGD(self.parameters(), lr=0.001, weight_decay=0.001, momentum=0.9)
         elif self.opt_param == "adam":
             optimizers = torch.optim.Adam(self.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
