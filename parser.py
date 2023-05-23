611bc1e86ac25db282f156f801a37877a1c364bd
enricocapuano <80041693+enricocapuano@users.noreply.github.com>
2023-05-23 16:25:11 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -51,7 +51,7 @@ def parse_arguments():
                         help="type of loss function")
     parser.add_argument("--pool", type=str, default="gem",
                         help="type of pooling")
-    parser.add_argument("--miner", type=str, default="ms",
+    parser.add_argument("--miner", type=str, default=None,
                         help="type of miner")
     
     # Visualizations parameters
